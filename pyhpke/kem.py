4fc068be3a9e51aba3b57ea54e0d834ad689718c
Ole Hannemann <ole.hannemann@stadtwerke-schwedt.de>
2023-10-26 10:39:01 +0200

    python 3.8 compatibility

--- pyhpke/kem.py
+++ pyhpke/kem.py
@@ -56,7 +56,9 @@ class KEM(KEMInterface):
 
     def extract_and_expand(self, dh: bytes, kem_context: bytes, length: int) -> bytes:
         eae_prk = self._kdf.labeled_extract(b"", b"eae_prk", dh)
-        shared_secret = self._kdf.labeled_expand(eae_prk, b"shared_secret", kem_context, length)
+        shared_secret = self._kdf.labeled_expand(
+            eae_prk, b"shared_secret", kem_context, length
+        )
         return shared_secret
 
     def deserialize_private_key(self, key: bytes) -> KEMKeyInterface:
@@ -66,7 +68,10 @@ class KEM(KEMInterface):
         return self._prim.deserialize_public_key(key)
 
     def encap(
-        self, pkr: KEMKeyInterface, sks: Optional[KEMKeyInterface] = None, eks: Optional[KEMKeyPair] = None
+        self,
+        pkr: KEMKeyInterface,
+        sks: Optional[KEMKeyInterface] = None,
+        eks: Optional[KEMKeyPair] = None,
     ) -> Tuple[bytes, bytes]:
         """ """
         if eks is None:
@@ -84,12 +89,18 @@ class KEM(KEMInterface):
             dh2 = self._prim.exchange(sks, pkr)
             dh = dh1 + dh2
             pks = self._prim.derive_public_key(sks)
-            kem_context = enc + self._prim.serialize_public_key(pkr) + self._prim.serialize_public_key(pks)
+            kem_context = (
+                enc
+                + self._prim.serialize_public_key(pkr)
+                + self._prim.serialize_public_key(pks)
+            )
 
         shared_secret = self.extract_and_expand(dh, kem_context, self._nsecret)
         return shared_secret, enc
 
-    def decap(self, enc: bytes, skr: KEMKeyInterface, pks: Optional[KEMKeyInterface] = None) -> bytes:
+    def decap(
+        self, enc: bytes, skr: KEMKeyInterface, pks: Optional[KEMKeyInterface] = None
+    ) -> bytes:
         """ """
         pke = self._prim.deserialize_public_key(enc)
         pkr = self._prim.derive_public_key(skr)
@@ -100,7 +111,11 @@ class KEM(KEMInterface):
             dh1 = self._prim.exchange(skr, pke)
             dh2 = self._prim.exchange(skr, pks)
             dh = dh1 + dh2
-            kem_context = enc + self._prim.serialize_public_key(pkr) + self._prim.serialize_public_key(pks)
+            kem_context = (
+                enc
+                + self._prim.serialize_public_key(pkr)
+                + self._prim.serialize_public_key(pks)
+            )
 
         shared_secret = self.extract_and_expand(dh, kem_context, self._nsecret)
         return shared_secret
@@ -109,7 +124,10 @@ class KEM(KEMInterface):
         kdf = self._kdf
         old_nsecret = self._nsecret
 
-        if self.id == KEMId.DHKEM_P256_HKDF_SHA256 or self.id == KEMId.DHKEM_P384_HKDF_SHA384:
+        if (
+            self.id == KEMId.DHKEM_P256_HKDF_SHA256
+            or self.id == KEMId.DHKEM_P384_HKDF_SHA384
+        ):
             sk_raw = _ec_derive_key_pair(ikm, kdf, self)
         elif self.id == KEMId.DHKEM_P521_HKDF_SHA512:
             self._nsecret = 66
@@ -142,25 +160,28 @@ def _ec_derive_key_pair(ikm: bytes, kdf: KDF, kem: KEM) -> bytes:
     # see https://www.rfc-editor.org/rfc/rfc9180#section-7.1.3-4
 
     dkp_prk = kdf.labeled_extract(b"", b"dkp_prk", ikm)
-    match kem.id:
-        case KEMId.DHKEM_P256_HKDF_SHA256:
-            bitmask = 0xFF
-            order = 0xFFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC632551
-        case KEMId.DHKEM_P384_HKDF_SHA384:
-            bitmask = 0xFF
-            order = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC7634D81F4372DDF581A0DB248B0A77AECEC196ACCC52973
-        case KEMId.DHKEM_P521_HKDF_SHA512:
-            bitmask = 0x01
-            order = 0x01FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFA51868783BF2F966B7FCC0148F709A5D03BB5C9B8899C47AEBB6FB71E91386409
-        case _:
-            raise ValueError(f"Unknown KEMid {kem.id}")
+    if kem.id == KEMId.DHKEM_P256_HKDF_SHA256:
+        bitmask = 0xFF
+        order = 0xFFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC632551
+    elif kem.id == KEMId.DHKEM_P384_HKDF_SHA384:
+        bitmask = 0xFF
+        order = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC7634D81F4372DDF581A0DB248B0A77AECEC196ACCC52973
+    elif kem.id == KEMId.DHKEM_P521_HKDF_SHA512:
+        bitmask = 0x01
+        order = 0x01FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFA51868783BF2F966B7FCC0148F709A5D03BB5C9B8899C47AEBB6FB71E91386409
+    else:
+        raise ValueError(f"Unknown KEMid {kem.id}")
 
     sk = 0
     counter = 0
     while sk == 0 or sk >= order:
         if counter > 255:
             raise ValueError("could not derive keypair")
-        raw_key = bytearray(kdf.labeled_expand(dkp_prk, b"candidate", counter.to_bytes(1, "big"), kem._nsecret))
+        raw_key = bytearray(
+            kdf.labeled_expand(
+                dkp_prk, b"candidate", counter.to_bytes(1, "big"), kem._nsecret
+            )
+        )
 
         raw_key[0] = raw_key[0] & bitmask
         sk = int.from_bytes(raw_key)
